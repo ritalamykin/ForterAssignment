92587578111d0d26da5e0f04e76fa43ba728fb35
ritalamykin <ritalamykin1@gmail.com>
2022-08-26 10:48:20 +0300

    more

--- sellAllToAll/convert_csv_to_Forter_schema.py
+++ sellAllToAll/convert_csv_to_Forter_schema.py
@@ -49,30 +49,18 @@ def merge_file_into_json(json1, json2):
         tmp_iter = tmp_reader.iter(type=dict)
         input_iter = input_reader.iter(type=dict)
         (tmp, input) = ('', '')
+        tmp = next(tmp_iter, None)
+        input = next(input_iter, None)
         while tmp != None and input != None:
-            tmp = next(tmp_iter, None)
-            input = next(input_iter, None)
-            if tmp["orderId"] == input["orderId"]:
-                break
-            else:
-                line_to_write = add_default_entries(input)
-                writer.write(line_to_write)
-                input = next(input_iter, None)
-        while tmp != None and input != None:
-            if tmp["orderId"] == input["orderId"]:
+            while tmp != None and input != None and tmp["orderId"] == input["orderId"]:
                 writer.write(inner_join_entries(tmp, input, 'orderId'))
-                tmp = next(tmp_iter, None)
+                curr_orderId = tmp['orderId']
                 input = next(input_iter, None)
-            elif tmp["orderId"] < input["orderId"]:
-                while (tmp != None and tmp["orderId"] < input["orderId"]):
-                    line_to_write = add_default_entries(tmp)
-                    writer.write(line_to_write)
-                    tmp = next(tmp_iter, None)
-            else:
-                while (input != None and tmp["orderId"] > input["orderId"]):
-                    line_to_write = add_default_entries(input)
+                while (input != None and curr_orderId == input['orderId']):
+                    line_to_write = inner_join_entries(tmp, input, 'orderId')
                     writer.write(line_to_write)
                     input = next(input_iter, None)
+                tmp = next(tmp_iter, None)
         # Dump last entries
         while (input != None):
             line_to_write = add_default_entries(input)
@@ -84,26 +72,27 @@ def merge_file_into_json(json1, json2):
             tmp = next(tmp_iter, None)
 
 
-def inner_join_entries(tmp, input, idenifier_field):
-    input.pop(idenifier_field)
-    tmp.update(input)
-    return tmp
-
-
 def add_default_entries(line):
     identifier = determine_identifier(line)
     orders_main = (
-        'checkoutTime', 'amountUSD', 'name', 'quantity', 'price')
+        'checkoutTime', 'name', 'quantity', 'price')
     accounts = (
-        'firstName', 'lastName', 'email', 'accountId', 'amountUSD')
+        'firstName', 'lastName', 'email', 'accountId')
     # Case 1/2 - add orders_main/accounts as empty data
     if (identifier):
         inner_dic = {field: 0 for field in orders_main}
         line['cartItems'] = inner_dic
     else:
+        line['amountUSD'] = 0
         inner_dic = {field: 0 for field in accounts}
         line['accountOwner'] = inner_dic
     return line
+
+
+def inner_join_entries(tmp, input, idenifier_field):
+    input.pop(idenifier_field)
+    tmp.update(input)
+    return tmp
 
 
 def determine_identifier(line):
@@ -141,12 +130,30 @@ def swap_field_name(row_dict, old_field, new_field):
         row_dict[new_field] = row_dict.pop(old_field)
 
 
+def identify_fraudulent_in_json(json_path):
+    with jsonlines.open(json_path) as reader, jsonlines.open('fraudulent_activity.jsonl', mode='w') as writer:
+        json_iter = reader.iter(type=dict)
+        entry = next(json_iter)
+        while entry != None:
+            curr_id = entry['orderId']
+            total_amount = int(entry['amountUSD'])
+            transaction_price_sum = float(
+                entry['cartItems'][0]['price']) * float(entry['cartItems'][0]['quantity'])
+            while entry != None and curr_id == entry['orderId']:
+                transaction_price_sum += float(entry['cartItems'][0]['price']) * float(
+                    entry['cartItems'][0]['quantity'])
+                entry = next(json_iter, None)
+            if total_amount > transaction_price_sum:
+                writer.write(curr_id)
+
+
 def main():
     convert_csv_to_Forter_schema_for_sellAllToAll(
         "/Users/ritalamykin/Desktop/ForterAssignment/sellAllToAll/sellAllToAll_accounts.csv", "temp1.jsonl")
     convert_csv_to_Forter_schema_for_sellAllToAll(
         "/Users/ritalamykin/Desktop/ForterAssignment/sellAllToAll/sellAllToAll_orders_main.csv", 'temp2.jsonl')
     merge_file_into_json("temp1.jsonl", "temp2.jsonl")
+    identify_fraudulent_in_json("final.jsonl")
 
 
 if __name__ == "__main__":
